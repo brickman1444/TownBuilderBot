a0e711114d2407e197d82fdd24f17f1d5c6f6439
Zac Gross <brickman1444@gmail.com>
2020-05-25 18:13:27 -0500

    Copy a bunch of files from CaptainRexEbooks to get started.

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace TownBuilderBot
+{
+    static class Program
+    {
+        public static Stream awsLambdaHandler(Stream inputStream)
+        {
+            Console.WriteLine("starting via lambda");
+            Main(new string[0]);
+            return inputStream;
+        }
+
+        public class CustomJsonLanguageConverter : Tweetinvi.Logic.JsonConverters.JsonLanguageConverter
+        {
+            public override object ReadJson(Newtonsoft.Json.JsonReader reader, Type objectType, object existingValue, Newtonsoft.Json.JsonSerializer serializer)
+            {
+                return reader.Value != null
+                    ? base.ReadJson(reader, objectType, existingValue, serializer)
+                    : Tweetinvi.Models.Language.English;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Beginning program");
+
+            GenerateQuoteAndTweet();
+        }
+
+        static void GenerateQuoteAndTweet()
+        {
+            InitializeTwitterCredentials();
+
+            Tweetinvi.Logic.JsonConverters.JsonPropertyConverterRepository.JsonConverters.Remove(typeof(Tweetinvi.Models.Language));
+            Tweetinvi.Logic.JsonConverters.JsonPropertyConverterRepository.JsonConverters.Add(typeof(Tweetinvi.Models.Language), new CustomJsonLanguageConverter());
+
+            TweetQuote("Test tweet");
+        }
+
+        static void InitializeTwitterCredentials()
+        {
+            string consumerKey = System.Environment.GetEnvironmentVariable("twitterConsumerKey");
+            string consumerSecret = System.Environment.GetEnvironmentVariable("twitterConsumerSecret");
+            string accessToken = System.Environment.GetEnvironmentVariable("twitterAccessToken");
+            string accessTokenSecret = System.Environment.GetEnvironmentVariable("twitterAccessTokenSecret");
+
+            if (consumerKey == null)
+            {
+                using (StreamReader fs = File.OpenText("localconfig/twitterKeys.txt"))
+                {
+                    consumerKey = fs.ReadLine();
+                    consumerSecret = fs.ReadLine();
+                    accessToken = fs.ReadLine();
+                    accessTokenSecret = fs.ReadLine();
+                }
+            }
+
+            Tweetinvi.Auth.SetUserCredentials(consumerKey, consumerSecret, accessToken, accessTokenSecret);
+        }
+
+        static void TweetQuote(string quote)
+        {
+            Console.WriteLine("Publishing tweet: " + quote);
+            var tweet = Tweetinvi.Tweet.PublishTweet(quote);
+        }
+    }
+}

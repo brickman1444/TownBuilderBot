692ed9c67fe0dc8e93e23b96e9a9fb37cc1f22af
Zac Gross <brickman1444@gmail.com>
2022-12-04 21:04:05 -0600

    Implement ReplaceElement() for Emoji

--- test/Tests.cs
+++ test/Tests.cs
@@ -6,19 +6,21 @@ namespace TownBuilderBot
 {
     public static class TestExtensionMethods
     {
-        public static string ReplaceElement(string grid, int width, int x, int y, string newString)
+        public static string ReplaceElement(string inGrid, int width, int x, int y, string newString)
         {
             int index = y * (width + 1) + x;
 
-            string prefix = grid.Substring(0, index);
+            System.Globalization.StringInfo stringInfo = new System.Globalization.StringInfo(inGrid);
 
-            string suffix = grid.Substring(index + 1);
+            string prefix = stringInfo.SubstringByTextElements(0, index);
+
+            string suffix = stringInfo.SubstringByTextElements(index + 1);
 
             return prefix + newString + suffix;
         }
 
         [Fact]
-        public static void ReplaceElement_Test()
+        public static void ReplaceElement_OnlyChangesTargetElement()
         {
             string grid = "ABCD\n"
                         + "EFGH\n"
@@ -31,6 +33,24 @@ namespace TownBuilderBot
                                   + "EFGH\n"
                                   + "IXKL\n"
                                   + "MNOP";
+
+            Assert.Equal(result, expectedResult);
+        }
+
+        [Fact]
+        public static void ReplaceElement_WorksWithEmoji()
+        {
+            string grid = "🏔🏔🏔🏔\n"
+                        + "🏔🏔🏔🏔\n"
+                        + "🏔🏔🏔🏔\n"
+                        + "🏔🏔🏔🏔";
+
+            string result = ReplaceElement(grid, 4, 1, 2, "X");
+
+            string expectedResult = "🏔🏔🏔🏔\n"
+                                  + "🏔🏔🏔🏔\n"
+                                  + "🏔X🏔🏔\n"
+                                  + "🏔🏔🏔🏔";
 
             Assert.Equal(result, expectedResult);
         }
